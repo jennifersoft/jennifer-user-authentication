e520c4becb5eaca07a663ea8de1b2224e6da8b81
alvin.hhh <alvin@jennifersoft.com>
2022-05-24 14:29:09 +0900

    SSO 어댑터 연동 + 사용자 정의 대시보드 Iframe 모드 샘플 코드 추가 #2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { HOST, USER_ID, USER_PASSWORD, DASHBOARD_URL_PREFIX, DASHBOARD_LIST } from "./metadata";
+import { HOST, USER_ID, USER_PASSWORD, DASHBOARD_LIST } from "./metadata";
 
 const Home: NextPage = () => {
-    const url = `${HOST}/login/sso?id=${USER_ID}&password=${USER_PASSWORD}&redirect=${encodeURIComponent(DASHBOARD_URL_PREFIX + DASHBOARD_LIST[0])}`;
+    const prefix = '/userdefine/dashboard?layout=iframe&key=';
+    const url = `${HOST}/login/sso?id=${USER_ID}&password=${USER_PASSWORD}&redirect=${encodeURIComponent(prefix + DASHBOARD_LIST[0])}`;
 
     return (
         <div className={styles.container}>
